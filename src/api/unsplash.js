a830233e44194b52a1da3fa0a21e48c4a595d07d
Mikele Corleone <sellmar38@gmail.com>
2023-04-22 13:39:53 +1000

    access-token

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import { createApi } from "unsplash-js";
 const ACCESS = import.meta.env.VITE_ACCESS_KEY;
 export const authRequest = () => {
   return createApi({
-    accessKey: ACCESS,
+    accessKey: "oNgDMSlqsaT4bLMGVRNwNsoW4pbjBwm5lYFyDkq30MI",
   });
 };
