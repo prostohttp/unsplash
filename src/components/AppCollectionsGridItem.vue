88371690dcae9454442e0c115291a81c2a22c025
Mikele Corleone <sellmar38@gmail.com>
2023-04-26 19:38:37 +1000

    Фикс отсутствия картинок в коллекции

--- src/components/AppCollectionsGridItem.vue
+++ src/components/AppCollectionsGridItem.vue
@@ -25,33 +25,46 @@ const { item } = defineProps({
           collection: item.title,
         },
       }"
-      v-if="item.preview_photos.length > 3"
+      v-if="item.preview_photos"
       class="grid h-[250px] w-full grid-cols-3 grid-rows-2 gap-[3px] overflow-hidden rounded-[6px]"
     >
       <img
-        :src="item.preview_photos[0].urls.small"
+        v-if="item.preview_photos[0]"
+        :src="item.preview_photos[0] ? item.preview_photos[0].urls.small : null"
         :alt="item.title"
         class="col-span-2 row-span-2 h-full w-full object-cover object-center"
       />
+      <div v-else class="h-full w-full bg-[#f6f6f6]"></div>
       <img
-        :src="item.preview_photos[1].urls.small"
+        v-if="item.preview_photos[1]"
+        :src="item.preview_photos[1] ? item.preview_photos[1].urls.small : null"
         :alt="item.title"
         class="col-span-1 row-span-1 h-full w-full object-cover object-center"
       />
+      <div v-else class="h-full w-full bg-[#f6f6f6]"></div>
       <img
-        :src="item.preview_photos[2].urls.small"
+        v-if="item.preview_photos[2]"
+        :src="item.preview_photos[2] ? item.preview_photos[2].urls.small : null"
         :alt="item.title"
         class="col-span-1 row-span-1 h-full w-full object-cover object-center"
       />
+      <div v-else class="h-full w-full bg-[#f6f6f6]"></div>
     </router-link>
-    <template v-else>
-      <img
-        :src="item.preview_photos[0].urls.small"
-        :alt="item.title"
-        class="h-[250px] w-full rounded-[6px] object-cover object-center"
-      />
-    </template>
-
+    <router-link
+      v-else
+      :to="{
+        name: 'profile-collection',
+        params: {
+          user: item.user.username,
+          collection: item.title,
+        },
+      }"
+      class="grid h-[250px] w-full grid-cols-3 grid-rows-2 gap-[3px] overflow-hidden rounded-[6px]"
+    >
+      <div class="col-span-2 row-span-2 h-full w-full bg-[#f6f6f6]"></div>
+      <div class="col-span-1 row-span-1 h-full w-full bg-[#f6f6f6]"></div>
+      <div class="col-span-1 row-span-1 h-full w-full bg-[#f6f6f6]"></div>
+    </router-link>
     <span class="mt-[15px] block text-[18px] font-semibold text-body">
       {{ item.title }}
     </span>
@@ -71,7 +84,7 @@ const { item } = defineProps({
     <div class="flex flex-wrap gap-[9px]" v-if="item.tags.length">
       <span
         v-for="tag in item.tags"
-        class="flex rounded-[2px] bg-[#eee] px-[8px] text-[14px] text-[#767676]"
+        class="flex rounded-[2px] bg-[#eee] px-[8px] text-[14px] capitalize text-[#767676]"
       >
         {{ tag.title }}
       </span>
