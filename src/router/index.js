10ff6117fd0144b25be732247a92d345e064b2c1
Morozov Mikhail <memphis.sail@gmail.com>
2024-03-23 17:44:38 +1000

    FIX: diff typo and names files fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { routes } from "@/router/routes.ts";
+import { routes } from "@/router/routes";
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
