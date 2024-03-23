77e4650e9f20467bfe85a6cc4c620b23e5de2550
Morozov Mikhail <memphis.sail@gmail.com>
2024-03-23 12:10:20 +1000

    FIX: small changes

--- src/components/AppPhotosGrid.vue
+++ src/components/AppPhotosGrid.vue
@@ -1,37 +1,37 @@
 <script setup>
-import AppPhotosGridItem from "@/components/AppPhotosGridItem.vue";
+	import AppPhotosGridItem from "@/components/AppPhotosGridItem.vue";
 
-//Stores
-// Vars
-const { items, route } = defineProps({
-  items: {
-    type: Array,
-    required: true,
-  },
-  route: {
-    type: Object,
-    required: true,
-  },
-});
+	//Stores
+	// Vars
+	const { items, route } = defineProps({
+		items: {
+			type: Array,
+			required: true,
+		},
+		route: {
+			type: Object,
+			required: true,
+		},
+	});
 
-// Handlers
-// Hooks
+	// Handlers
+	// Hooks
 </script>
 
 <template>
-  <div
-    v-masonry
-    transition-duration="0.3s"
-    item-selector=".item"
-    class="max-w-[1280px]"
-    gutter="25"
-  >
-    <AppPhotosGridItem
-      :route="route"
-      v-masonry-tile
-      class="item mb-[20px]"
-      v-for="item in items"
-      :item="item"
-    />
-  </div>
+	<div
+		v-masonry
+		transition-duration="0.3s"
+		item-selector=".item"
+		class="max-w-[1280px]"
+		gutter="25"
+	>
+		<AppPhotosGridItem
+			:route="route"
+			v-masonry-tile
+			class="item mb-[20px]"
+			v-for="item in items"
+			:item="item"
+		/>
+	</div>
 </template>
