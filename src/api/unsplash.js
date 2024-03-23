cd62f9707ce0159e715f35f4f6311cbdd092308c
Morozov Mikhail <memphis.sail@gmail.com>
2024-03-23 14:07:14 +1000

    FIX: token

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import { createApi } from "unsplash-js";
 const ACCESS = import.meta.env.VITE_ACCESS_KEY;
 export const authRequest = () => {
   return createApi({
-    accessKey: "oNgDMSlqsaT4bLMGVRNwNsoW4pbjBwm5lYFyDkq30MI",
+    accessKey: ACCESS,
   });
 };
